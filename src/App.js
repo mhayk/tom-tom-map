a529b9e828471fed4c22547fb0ba9a2a51b81f13
Mhayk Whandson <hi@mhayk.com>
2021-06-08 18:37:40 +0100

    Map from TomTom is being showed. \o/
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { useEffect, useRef, useState } from 'react';
+import * as tt from '@tomtom-international/web-sdk-maps'
 import './App.css'
 
 function App() {
+  const mapElement = useRef()
+  const [map, setMap] = useState({})
+
+  useEffect(() => {
+    let map = tt.map({
+      key: process.env.REACT_APP_TOM_TOM_API_KEY,
+      container: mapElement.current,
+    })
+
+    setMap(map)
+
+  }, [])
+
   return (
     <div className="App">
+      <div ref={mapElement}></div>
     </div>
   );
 }
