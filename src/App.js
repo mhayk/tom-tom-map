881ddf43e20de6d2ab5dae4ebed3538fe3bcb751
Mhayk Whandson <hi@mhayk.com>
2021-06-08 19:02:00 +0100

    Longitude and latitue have been set and stylesVisibility from the map has been set too.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import './App.css'
 function App() {
   const mapElement = useRef()
   const [map, setMap] = useState({})
+  const [longitude, setLongitude] = useState(-0.34675)
+  const [latitude, setLatitude] = useState(51.46664)
 
   useEffect(() => {
     let map = tt.map({
       key: process.env.REACT_APP_TOM_TOM_API_KEY,
       container: mapElement.current,
+      stylesVisibility: {
+        trafficIncidents: true,
+        trafficFlow: true
+      },
+      center: [longitude, latitude],
+      zoom: 14
     })
 
     setMap(map)
